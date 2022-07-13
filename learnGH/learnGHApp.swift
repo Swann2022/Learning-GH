65f837a5bea368b35f3e559f61079e30c14a6a9e
Swann <eeli@lionconsulting.io>
2022-07-13 16:56:51 +0300

    Initial Commit

---
+++ learnGH/learnGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnGHApp.swift
+//  learnGH
+//
+//  Created by Eeli Lohi on 13.7.2022.
+//
+
+import SwiftUI
+
+@main
+struct learnGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
